a3aba9763167f4a0fa5e5e1a0b5fd100ab9a9cb8
Cloud User <ec2-user@ip-172-31-40-77.ap-south-1.compute.internal>
2026-03-30 23:41:52 +0000

    added C project

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// function declarations
+void biggest3();
+void factorial();
+void palindrome();
+
+int main() {
+    biggest3();
+    factorial();
+    palindrome();
+    return 0;
+}
